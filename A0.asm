a4abdca326aa38cc8825e48a1ad6503dc73a8aa6
sridhar-sodhi-21104 <108402101+sridhar-sodhi-21104@users.noreply.github.com>
2022-09-20 19:15:42 +0530

    Add files via upload

---
+++ A0.asm
@@ -0,0 +1,37 @@
+global main
+extern printf,scanf,exit
+
+section.text
+main:
+    push rbp
+    lea rdi,[inp]
+    lea rsi[num]
+    xor eax,eax
+    call scanf
+
+    lea rdi,[inps]
+    lea rsi[s]
+    xor eax,eax
+    call scanf
+
+    lea rdi,[string1]
+    mov rsi,[num]
+    xor eax,eax
+    call printf
+
+    lea rdi,[string2]
+    lea rsi,[s]
+    xor eax,eax
+    call printf
+    pop rbp
+    xor eax,eax
+    call exit
+
+section.data
+
+inp db "%d",0
+inps db "%s",0
+string2 db "Entered string is : %s",10,0
+string1 db "Entered number is : %d",10,0
+num dd 0
+s times 100 db 0
